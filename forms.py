55a7b7af275fb44a7c8da14bc9bebf81e75870b1
venomslasher <gokul.3099@gmail.com>
2023-08-21 20:40:14 +0530

    functional_form_structure

--- forms.py
+++ forms.py
@@ -1,22 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, SubmitField, BooleanField  
+from wtforms import StringField,IntegerField, FloatField, SubmitField, BooleanField, SelectMultipleField
 from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
-class PreInputForm(FlaskForm):
-    imput_file = FileField('Data File',validators = [FileRequired()])
-    # lower_wave = FloatField('lower wavelength',validators = [DataRequired()])
-    # upper_wave = FloatField('lower wavelength', validators = [DataRequired()])
-    # baseline_intensity = FloatField('baseline_intensity',validators = [DataRequired()])
-    submit = SubmitField('Submit')
+element_symbols = {
+    'H': 'Hydrogen', 'He': 'Helium', 'Li': 'Lithium', 'Be': 'Beryllium', 'B': 'Boron', 'C': 'Carbon',
+    'N': 'Nitrogen', 'O': 'Oxygen', 'F': 'Fluorine', 'Ne': 'Neon', 'Na': 'Sodium', 'Mg': 'Magnesium',
+    'Al': 'Aluminum', 'Si': 'Silicon', 'P': 'Phosphorus', 'S': 'Sulfur', 'Cl': 'Chlorine', 'Ar': 'Argon',
+    'K': 'Potassium', 'Ca': 'Calcium', 'Sc': 'Scandium', 'Ti': 'Titanium', 'V': 'Vanadium', 'Cr': 'Chromium',
+    'Mn': 'Manganese', 'Fe': 'Iron', 'Co': 'Cobalt', 'Ni': 'Nickel', 'Cu': 'Copper', 'Zn': 'Zinc',
+    'Ga': 'Gallium', 'Ge': 'Germanium', 'As': 'Arsenic', 'Se': 'Selenium', 'Br': 'Bromine', 'Kr': 'Krypton',
+    'Rb': 'Rubidium', 'Sr': 'Strontium', 'Y': 'Yttrium', 'Zr': 'Zirconium', 'Nb': 'Niobium', 'Mo': 'Molybdenum',
+    'Tc': 'Technetium', 'Ru': 'Ruthenium', 'Rh': 'Rhodium', 'Pd': 'Palladium', 'Ag': 'Silver', 'Cd': 'Cadmium',
+    'In': 'Indium', 'Sn': 'Tin', 'Sb': 'Antimony', 'Te': 'Tellurium', 'I': 'Iodine', 'Xe': 'Xenon',
+    'Cs': 'Cesium', 'Ba': 'Barium', 'La': 'Lanthanum', 'Ce': 'Cerium', 'Pr': 'Praseodymium', 'Nd': 'Neodymium',
+    'Pm': 'Promethium', 'Sm': 'Samarium', 'Eu': 'Europium', 'Gd': 'Gadolinium', 'Tb': 'Terbium',
+    'Dy': 'Dysprosium', 'Ho': 'Holmium', 'Er': 'Erbium', 'Tm': 'Thulium', 'Yb': 'Ytterbium', 'Lu': 'Lutetium',
+    'Hf': 'Hafnium', 'Ta': 'Tantalum', 'W': 'Tungsten', 'Re': 'Rhenium', 'Os': 'Osmium', 'Ir': 'Iridium',
+    'Pt': 'Platinum', 'Au': 'Gold', 'Hg': 'Mercury', 'Tl': 'Thallium', 'Pb': 'Lead', 'Bi': 'Bismuth',
+    'Po': 'Polonium', 'At': 'Astatine', 'Rn': 'Radon', 'Fr': 'Francium', 'Ra': 'Radium', 'Ac': 'Actinium',
+    'Th': 'Thorium', 'Pa': 'Protactinium', 'U': 'Uranium', 'Np': 'Neptunium', 'Pu': 'Plutonium',
+    'Am': 'Americium', 'Cm': 'Curium', 'Bk': 'Berkelium', 'Cf': 'Californium', 'Es': 'Einsteinium',
+    'Fm': 'Fermium', 'Md': 'Mendelevium', 'No': 'Nobelium', 'Lr': 'Lawrencium'
+}
 
 class InputForm(FlaskForm):
-    imput_file = FileField('Data File',validators = [FileRequired()])
+    input_file = FileField('Data File',validators = [FileAllowed(['csv'])])
     lower_wave = FloatField('lower wavelength',validators = [DataRequired()])
-    upper_wave = FloatField('lower wavelength', validators = [DataRequired()])
+    upper_wave = FloatField('upper wavelength', validators = [DataRequired()])
     baseline_intensity = FloatField('baseline_intensity',validators = [DataRequired()])
+    r_cutoff = FloatField('right cutoff',validators = [DataRequired()])
+    l_cutoff = FloatField('left cutoff',validators = [DataRequired()])
+    n_peaks = IntegerField('no. of peaks to match',validators = [DataRequired()])
+    selected_elements = SelectMultipleField('Select Elements', choices=[(symbol, element_symbols[symbol]) for symbol in element_symbols.keys()])
+    PS = BooleanField('P')
     submit = SubmitField('Submit')
-    PS = BooleanField('')
+    
 
 
 
--- web_server.py
+++ web_server.py
@@ -1,20 +1,62 @@
-from flask import Flask, render_template, url_for
-from forms import PreInputForm, InputForm 
-
+from flask import Flask, request, render_template, url_for, redirect, session
+from forms import  InputForm , element_symbols
+import os
+import datetime as dt
+import secrets
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '73e2889840a574502969a1ad279ef26f'
 
+UPLOAD_FOLDER = os.path.join(app.root_path, 'Files_toprocess')
 
+def save_file(form_file):
+    current_time = dt.datetime.now()
+    formatted_time = current_time.strftime('%y%m%d%H%M%S')
+    filename = secure_filename(form_file.filename)
+    if (not os.path.isdir(os.path.join(app.root_path, 'Files_toprocess',session['uid']))):
+        os.mkdir(os.path.join(app.root_path, 'Files_toprocess',session['uid']))
+    f_name, f_ext = os.path.splitext(filename)
+    fname = f_name+"_"+ formatted_time+f_ext
+    path_file = os.path.join(app.root_path, 'Files_toprocess',session['uid'], fname)
+    form_file.save(path_file)
+    return path_file
+    
+    
 
-@app.route('/')
-@app.route('/home')
+    
+
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    pre_form = PreInputForm()
+    form = InputForm()            
+    if form.validate_on_submit():
+        if form.input_file.data:
+            uploaded_file = form.input_file.data
+            file_path = save_file(uploaded_file)
+            if 'file_list' in session:
+                session['file_list'].append(file_path)
+                return redirect(url_for('results'))
+            session['file_list'] = list(file_path)        
+            return redirect(url_for('results'))
+        return render_template('home.html', form =form, sess = session['uid'])
+
+    if request.method =='GET':
+        if session.new:
+            session['uid'] = secrets.token_hex(8)
+        elif (not session.new):
+            if 'uid' in session:
+                pass
+            else:
+                session['uid'] = secrets.token_hex(8)            
+        return render_template('home.html', form =form, sess = session['uid'])
+    return render_template('home.html', form =form, sess = session['uid'])
+
+@app.route('/homed3')
+def homed3():
+    form = InputForm()
     # form = InputForm()
 
-    return render_template('home.html', pre_form =pre_form)
-
+    return render_template('homed3.html', form =form)
 
 @app.route('/results')
 def results():
@@ -32,4 +74,6 @@ def about():
 
 
 if __name__ == '__main__':
+    if (not os.path.isdir(os.path.join(app.root_path, 'Files_toprocess'))):
+        os.mkdir(os.path.join(app.root_path, 'Files_toprocess'))
     app.run(debug=True)
